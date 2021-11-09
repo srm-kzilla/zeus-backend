33dcf7eacd9305180917f0d4a9147f2266b1a253
Rahul Sharma <90137rahul@gmail.com>
2021-11-09 22:08:22 +0530

    💡 Add comments & refactored code

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/joho/godotenv"
 	api "github.com/srm-kzilla/events/api"
 )
 
@@ -17,13 +20,43 @@ func setupRoutes(app *fiber.App) {
 	api.SetupApp(app)
 }
 
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Panicln(err)
+	}
+}
+
 func main() {
 	log.Println("Server Starting!!!")
 	app := fiber.New()
+
+	// Creating a logger middleware
+	app.Use(logger.New())
+
+	//setting up cors
+	app.Use(cors.New())
+
+	// setting up api routes
 	setupRoutes(app)
+
+	//configuring godotenv package
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error Loading .env file")
+	}
+
+	//Setting up Port Value
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "3000"
 	}
+
+	log.Printf(`
+	################################################
+	🛡️  Server listening on port: %s 🛡️
+	################################################
+  `, port)
+
 	app.Listen(":" + port)
 }
